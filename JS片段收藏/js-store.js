678f74b35796408fa35381fff476d4f36f382c1d
lizhenfei <thahtakmen@163.com>
2018-07-20 16:29:12 +0800

    添加犀牛书

---
+++ JS片段收藏/js-store.js
@@ -0,0 +1,11 @@
+// HTML 字符转义
+function htmlEscape(text){
+    return text.replace(/[<>"&]/g,function(match,pos,originalText){
+        switch(match){
+            case "<":return "&lt;";
+            case ">":return "&gt;";
+            case "&":return "&amp;";
+            case "\"":return "&quot;";
+        }
+    })
+}
